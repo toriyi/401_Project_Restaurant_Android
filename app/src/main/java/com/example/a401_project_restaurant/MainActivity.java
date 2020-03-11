8ec54d621b784b4d81f2a7e92830d8563f4df3b2
toriyi <toriyi@usc.edu>
2020-03-10 21:44:55 -0700

    committing sign up, favorites, and description page

--- app/src/main/java/com/example/a401_project_restaurant/MainActivity.java
+++ app/src/main/java/com/example/a401_project_restaurant/MainActivity.java
@@ -2,12 +2,28 @@ package com.example.a401_project_restaurant;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.view.View;
+import android.widget.ImageView;
+import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.login);
+
+        Button btn = (Button)findViewById(R.id.button);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, Favorites.class));
+            }
+        });
+
     }
+
+
 }
---
+++ app/src/main/java/com/example/a401_project_restaurant/ResInfo.java
@@ -0,0 +1,12 @@
+package com.example.a401_project_restaurant;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class ResInfo extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.resinfo);
+    }
+}
