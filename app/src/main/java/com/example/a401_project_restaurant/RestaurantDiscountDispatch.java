d1db0542e3c75970481275fe4b9259f95d70da5d
Jenna Permut <permut@usc.edu>
2020-03-10 22:44:41 -0700

    initial commit! ui layout

---
+++ app/src/main/java/com/example/a401_project_restaurant/RestaurantDiscountDispatch.java
@@ -0,0 +1,13 @@
+package com.example.a401_project_restaurant;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RestaurantDiscountDispatch extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_restaurant_discount_dispatch);
+    }
+}
