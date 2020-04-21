c3e118213416941c3702029d566326c95e2db143
toriyi <toriyi@usc.edu>
2020-04-20 18:42:45 -0700

    committing updated sign up, login

---
+++ app/src/main/java/com/example/a401_project_restaurant/Login.java
@@ -0,0 +1,83 @@
+package com.example.a401_project_restaurant;
+
+import android.content.Intent;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+
+public class Login extends AppCompatActivity {
+
+    private EditText emailU, passwordU;
+    private Button loginBtn;
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        initializeUI();
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loginUserAccount();
+            }
+        });
+    }
+
+    private void loginUserAccount() {
+
+        String email, password;
+        email = emailU.getText().toString();
+        password = passwordU.getText().toString();
+
+        if (TextUtils.isEmpty(email)) {
+            Toast.makeText(getApplicationContext(), "Please enter email...", Toast.LENGTH_LONG).show();
+            return;
+        }
+        if (TextUtils.isEmpty(password)) {
+            Toast.makeText(getApplicationContext(), "Please enter password!", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(getApplicationContext(), "Login successful!", Toast.LENGTH_LONG).show();
+
+                            Intent intent = new Intent(Login.this, Favorites.class);
+                            startActivity(intent);
+                        }
+                        else {
+                            Toast.makeText(getApplicationContext(), "Login failed! Please try again later", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+    }
+
+    private void initializeUI() {
+        emailU = findViewById(R.id.editText8);
+        passwordU = findViewById(R.id.editText7);
+
+        loginBtn = findViewById(R.id.buttonlog);
+    }
+
+
+}
--- app/src/main/java/com/example/a401_project_restaurant/ResInfo.java
+++ app/src/main/java/com/example/a401_project_restaurant/ResInfo.java
@@ -1,7 +1,7 @@
 package com.example.a401_project_restaurant;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class ResInfo extends AppCompatActivity {
     @Override
